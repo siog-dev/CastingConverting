fe666f59c53951fc14b36a55ac92bcc71b2338f6
siog <siog@LAPTOP-545TJ6PE.station>
2025-03-10 20:25:33 +0000

    Demonstrate numeric value of chars

--- CastingConverting/src/ie/atu/sw/Runner.java
+++ CastingConverting/src/ie/atu/sw/Runner.java
@@ -9,7 +9,17 @@ public class Runner {//runs the program
 		long l = (long) f; //A long is 64 bits
 		double d = l; //A 64 bit decimal number, precision of 16-19 p.o.d.
 		char c = (char) d; //A char has 16 bits
-		//add cast to coerce double to char
+		
+		System.out.println("Result: " + c);//prints: "Result: A"
+		c++;
+		System.out.println("Result: " + c);//prints: "Result: B"
+		
+		/*
+		 * 65 is A in ASCII
+		 * A + 1 = B
+		 * 
+		 */
+		
 	}
 
 }
