33f3642c3777957ac83933968694d0f23b4beeb1
siog <siog@LAPTOP-545TJ6PE.station>
2025-03-10 18:56:14 +0000

    Conversion error example, type mismatch

--- CastingConverting/src/ie/atu/sw/Runner.java
+++ CastingConverting/src/ie/atu/sw/Runner.java
@@ -5,8 +5,9 @@ public class Runner {//runs the program
 	public static void main(String[] args) {
 		byte b = 65; //8 bits in a byte
 		int i = b; //32 bits in an int
-		float f = i; //A float is 32 bits, has precision of 7-9 places of decimal
-		//java will convert the int to a float by adding ".0"
+		float f = i; //A float is 32 bits, has precision of 7-9 p.o.d
+		long l = f; //A long is 64 bits
+		//Type mismatch, cannot convert from float to long
 	}
 
 }
