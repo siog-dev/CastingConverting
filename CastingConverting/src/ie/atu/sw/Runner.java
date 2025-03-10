06146ebc4ad11087c3cf77eef62d35b49d845bfa
siog <siog@LAPTOP-545TJ6PE.station>
2025-03-10 19:54:02 +0000

    Add legal value to a new data type

--- CastingConverting/src/ie/atu/sw/Runner.java
+++ CastingConverting/src/ie/atu/sw/Runner.java
@@ -7,11 +7,7 @@ public class Runner {//runs the program
 		int i = b; //32 bits in an int
 		float f = i; //A float is 32 bits, has precision of 7-9 p.o.d
 		long l = (long) f; //A long is 64 bits
-		/*
-		 * Java will recommend coercion to resolve error
-		 * will not convert automatically without due to data loss
-		 * decimal points will be lost following coercion
-		 */
+		double d = l; //A 64 bit decimal number, precision of 16-19 p.o.d.
 	}
 
 }
