6d036fbe9f20fcab9e8788e3fb21e9faa3830a1f
siog <siog@LAPTOP-545TJ6PE.station>
2025-03-10 18:53:54 +0000

    Add data types, example of java data conversion

--- CastingConverting/src/ie/atu/sw/Runner.java
+++ CastingConverting/src/ie/atu/sw/Runner.java
@@ -3,7 +3,10 @@ package ie.atu.sw;
 public class Runner {//runs the program
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		byte b = 65; //8 bits in a byte
+		int i = b; //32 bits in an int
+		float f = i; //A float is 32 bits, has precision of 7-9 places of decimal
+		//java will convert the int to a float by adding ".0"
 	}
 
 }
