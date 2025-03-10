1ba434b1ab119d3b2de65664064a8f1e10e3c6c6
siog <siog@LAPTOP-545TJ6PE.station>
2025-03-10 19:59:15 +0000

    Add cast to resolve error

--- CastingConverting/src/ie/atu/sw/Runner.java
+++ CastingConverting/src/ie/atu/sw/Runner.java
@@ -8,11 +8,8 @@ public class Runner {//runs the program
 		float f = i; //A float is 32 bits, has precision of 7-9 p.o.d
 		long l = (long) f; //A long is 64 bits
 		double d = l; //A 64 bit decimal number, precision of 16-19 p.o.d.
-		char c = d; //A char has 16 bits
-		/*
-		 * Type mismatch: cannot convert from double to char
-		 * Not enough bits in a char to store the bits of the double
-		 */
+		char c = (char) d; //A char has 16 bits
+		//add cast to coerce double to char
 	}
 
 }
